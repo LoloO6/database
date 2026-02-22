d2dcffe09f1fb48bc306b57379212183780cbca7
LoloO6 <ml12365@auca.kg>
2026-02-22 16:06:05 +0600

    Databases

---
+++ lab05.sql
@@ -0,0 +1,10 @@
+postgres=#
+postgres=# CREATE DATABASE students;
+CREATE DATABASE
+postgres=# \c students
+You are now connected to database "students" as user "postgres".
+students=# \c postgres
+You are now connected to database "postgres" as user "postgres".
+postgres=# DROP DATABASE students;
+DROP DATABASE
+postgres=#
